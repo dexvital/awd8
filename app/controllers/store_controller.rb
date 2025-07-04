124bd749ceda7cbc1fbeba2ab959b9f6d44f5974
vitalijs.romanovskis <vitalijs.romanovskis@gmail.com>
2025-07-04 23:51:56 +0300

    14 Task I: Logging In

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,9 @@
 class StoreController < ApplicationController
+  allow_unauthenticated_access
+
   include CurrentCart
   before_action :set_cart
+
   def index
     @products = Product.order(:title)
   end
